fbc0b3e0ee8f1a6fe1aadea094a277d36064d940
Colin Rupp <colin@thinkful.com>
2019-01-30 12:06:30 -0800

    adds routes '/' and 'rsvp'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import {FormikRSVPForm} from './components/RSVPForm.js';
+import NavLinks from './components/NavLinks.js';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <h1>working</h1>
-        <FormikRSVPForm />
+        <NavLinks />
+        {/* <Route exact path='/' component={LandingPage} /> */}
+        <Route exact path='/rsvp' component={FormikRSVPForm} />
       </div>
+    </Router>
     );
   }
 }
