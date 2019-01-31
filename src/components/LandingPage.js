18bc3dbaa1e9ffc5c93a2a3e68777477e1000f01
Colin Rupp <colin@thinkful.com>
2019-01-30 22:06:33 -0800

    adds placeholders

---
+++ src/components/LandingPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/LandingPage.css';
+
+export default function LandingPage() {
+  return (
+    <main>
+      <div className="col-4">
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+        <h1>INVITE GOES HEAR</h1>
+      </div>
+      <div className="col-8">
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+        <h1>PICTURE OF US GOES HERE</h1>
+      </div>
+    </main>
+  )
+}
