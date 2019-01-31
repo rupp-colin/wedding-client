bced3e36d22420bed7938c070ab771271ee11acd
Colin Rupp <colin@thinkful.com>
2019-01-30 22:05:55 -0800

    imports stylesheet

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/NavLinks.css';
 
 export default function NavLinks() {
   return (
-    <nav>
-      <p><Link to='/'>Home</Link></p>
-      <p><Link to="/rsvp">RSVP</Link></p>
-      <p>About the Venue</p>
-      <p>Attire</p>
-      <p>Accomodations</p>
-      <p>Other Information</p>
+    <nav id="main-nav">
+      <div className="link-container"><Link to='/'>Home</Link></div>
+      <div className="link-container"><Link to="/rsvp">RSVP</Link></div>
+      <div className="link-container">Venue</div>
+      <div className="link-container">Attire</div>
+      <div className="link-container">Accomodations</div>
+      <div className="link-container">More Info</div>
     </nav>
   )
 }
