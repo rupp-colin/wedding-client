e6c215b0a9d9ac3b3a704921f8c9d06813726774
Colin Rupp <colin@thinkful.com>
2019-02-03 18:49:24 -0800

    updates App files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 
 import {FormikRSVPForm} from './components/RSVPForm.js';
 import NavLinks from './components/NavLinks.js';
 import LandingPage from './components/LandingPage.js';
+import VenueInfo from './components/VenueInfo.js';
 
 class App extends Component {
   render() {
@@ -13,8 +15,8 @@ class App extends Component {
         <NavLinks />
         <Route exact path='/' component={LandingPage} />
         <Route exact path='/rsvp' component={FormikRSVPForm} />
-        {/*<Route exact path='/venue' component={VenueInfo} />
-        <Route exact path='/attire' component={AttireInfo} />
+        <Route exact path='/venue' component={VenueInfo} />
+        {/*<Route exact path='/attire' component={AttireInfo} />
         <Route exact path='/accomodations' component={Accomodations} />
         <Route exact path='/otherinfo' component={OtherInfo} />*/}
       </div>
