35f97d869926876bed6ac9599e5f07ae87e22d60
Colin Rupp <colin@thinkful.com>
2019-02-03 18:51:02 -0800

    adds classNames for styling

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,16 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NavLinks.css';
 
-export default function NavLinks() {
+export default class NavLinks extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: false,
+    }
+  }
+
+  toggleMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
+  }
+
+  render() {
+    let menuToggler = this.state.showMenu ? "link-container col-2 menu-toggle-show" : "link-container col-2"
   return (
-    <nav id="main-nav">
-      <div className="link-container"><Link to='/'>Home</Link></div>
-      <div className="link-container"><Link to="/rsvp">RSVP</Link></div>
-      <div className="link-container">Venue</div>
-      <div className="link-container">Attire</div>
-      <div className="link-container">Accomodations</div>
-      <div className="link-container">More Info</div>
+    <nav id="main-nav" className="row">
+
+      <div
+        className="link-container menu-toggle"
+        onClick={() => this.toggleMenu()}
+      >
+        <i className="fas fa-bars"></i>
+      </div>
+
+      <div className={menuToggler}>
+        <Link to='/'>Home</Link>
+      </div>
+      <div className={menuToggler}>
+        <Link to="/rsvp">RSVP</Link>
+      </div>
+      <div className={menuToggler}>
+        <Link to="/venue">Venue</Link>
+      </div>
+      <div className={menuToggler}>
+        <Link to ="/hotels">Hotels</Link>
+      </div>
+      <div className={menuToggler}>
+        <Link to="moreinfo">More Info</Link>
+      </div>
     </nav>
   )
+  }
 }
