66d976d0561a69504e4a0c63d7cdcc2602260d16
Colin Rupp <colin@thinkful.com>
2019-02-07 21:32:35 -0800

    initial commit

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ComingSoon from './ComingSoon.js';
+import '../styles/Hotel.css';
 
-export default function Hotels() {
-  return <ComingSoon />
-}
+export default function Hotel(props) {
+  return (
+    <div className="hotel-container flex column col-6">
+      <h1>{props.hotelName}</h1>
+      <img src={props.hotelImage} alt={`picture of ${props.hotelName}`} />
+      <p>{props.hotelDesc}</p>
+      <a href={props.hotelLink}>Book here!</a>
+    </div>
+  )
+  }
