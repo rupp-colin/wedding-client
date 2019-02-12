5bf1081244c9a9e24c972a3b6c880bdbb4937fb2
Colin Rupp <colin@thinkful.com>
2019-02-11 19:59:28 -0800

    adds mock data

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -6,7 +6,10 @@ export default function Hotel(props) {
   return (
     <div className="hotel-container flex column col-6">
       <h1>{props.hotelName}</h1>
-      <img src={props.hotelImage} alt={`picture of ${props.hotelName}`} />
+      <img
+        src={props.hotelImage}
+        className="flex center"
+        alt={`picture of ${props.hotelName}`} />
       <p>{props.hotelDesc}</p>
       <a href={props.hotelLink}>Book here!</a>
     </div>
